6d41a3eb90ea6bcf5eeaa690167763bb5b537627
Luis Sacristán <luis.s@onthegosystems.com>
2025-10-02 21:39:23 +0200

    chore: added semantic release

--- release.config.js
+++ release.config.js
@@ -1,38 +1,51 @@
 module.exports = {
-  repositoryUrl: 'git@github.com:displaynone/lissn.git',
-  branches: [
-    'main',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true }
-  ],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md'
-      }
-    ],
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]'
-      }
-    ],
-    [
-      '@semantic-release/github',
-      {
-        successComment: false,
-        failComment: false
-      }
-    ]
-  ]
+	repositoryUrl: "git@github.com:displaynone/lissn.git",
+	branches: [
+		"main",
+		{ name: "beta", prerelease: true },
+		{ name: "alpha", prerelease: true },
+	],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/changelog",
+			{
+				changelogFile: "CHANGELOG.md",
+			},
+		],
+		[
+			"@semantic-release/npm",
+			{
+				npmPublish: false,
+			},
+		],
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]",
+			},
+		],
+		[
+			"@semantic-release/github",
+			{
+				successComment: false,
+				failComment: false,
+			},
+		],
+		[
+			"@semantic-release/commit-analyzer",
+			{
+				preset: "conventionalcommits",
+				releaseRules: [
+					{ type: "feature", release: "minor" },
+					{ type: "ui", release: "minor" },
+					{ type: "fix", release: "patch" },
+					{ type: "hotfix", release: "patch" },
+					{ type: "docs", release: false },
+				],
+			},
+		],
+	],
 };
